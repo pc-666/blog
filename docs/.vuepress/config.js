5cea0604ad78c5e8b2e1b3b456055d3f0d270f68
a153884673 <153884673@qq.com>
2023-02-27 22:16:56 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  title: '前端面试文档',
+  description: '',
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+        { text: '首页', link: '/' },
+        { 
+            text: 'oliver的 JavaScript 博客', 
+            items: [
+                { text: 'Github', link: 'https://github.com/oliver-xie666' },
+            ]
+        }
+    ],
+    sidebar: [
+      {
+          title: '欢迎学习',
+          path: '/',
+          collapsable: false, // 不折叠
+          children: [
+              { title: "学前必读", path: "/" }
+          ]
+      },
+      {
+        title: "HTML",
+        path: '/html/meta',
+        collapsable: true, // 不折叠
+        children: [
+          { title: "meta", path: "/html/meta" },
+        ],
+      }
+    ],
+    subSidebar: 'auto'
+  }
+}
